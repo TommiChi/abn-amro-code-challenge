78c42d2a3e04ddaadd78c2f89f8f5ccfe9241215
Tom Nchinda <tom@passionatepeople.io>
2025-04-04 15:26:45 +0200

    ✨feat(abnamro|ui): add basic scaffolding for all the pages and components I need

---
+++ src/components/SowDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    This is my product details component
+  </div>
+</template>
+
+<style>
+.login {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+}
+</style>
