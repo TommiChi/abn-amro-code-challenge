219373382380d553e908c8a2d92d8363fb4e3045
Tom Nchinda <tom@passionatepeople.io>
2025-04-04 15:27:49 +0200

    ✨feat(abnamro|auth): add Firebase authentication to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApavUqR6DH4c96Es63CeOz4OizYDT8FhM",
+  authDomain: "abn-amro-code-challenge-51ac2.firebaseapp.com",
+  projectId: "abn-amro-code-challenge-51ac2",
+  storageBucket: "abn-amro-code-challenge-51ac2.firebasestorage.app",
+  messagingSenderId: "85973761352",
+  appId: "1:85973761352:web:dc2e3ea1e61317a5ff877d",
+  measurementId: "G-Q1PCF6TQX4"
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +20,57 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requiresAuth: false,
+      },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
+      name: 'dashboard',
+      component: HomeView,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {
+        requiresAuth: false,
+      },
+      component: () => import('@/views/Login.vue'),
     },
   ],
-})
+});
 
-export default router
+router.beforeEach(async (to: RouteLocationNormalized, from, next) => {
+  initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const requiresAuth = to.matched.some((record) => {
+    console.warn('/!\\ RECORD:\n', record);
+    return record.meta.requiresAuth;
+  });
+
+  next();
+  if (requiresAuth) {
+    // Check if the user is authenticated
+    const user: User | null = await new Promise((resolve) => {
+      onAuthStateChanged(auth, (user) => {
+        resolve(user);
+      });
+    });
+
+    if (!user) {
+      // User is not logged in, redirect to login
+      next('/login');
+    } else {
+      // User is logged in, proceed to the route
+      next();
+    }
+  } else {
+    // Route does not require authentication, proceed
+    next();
+  }
+});
+
+export default router;
