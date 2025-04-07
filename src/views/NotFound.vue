44814c2c3c7cff5c665b0c5f66f09f40e0e4501e
Tom Nchinda <tom@passionatepeople.io>
2025-04-07 07:27:07 +0200

    ✨feat(abnamro|pages): add 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    This is my 404 page
+  </div>
+</template>
+
+<style>
+.login {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+}
+</style>
