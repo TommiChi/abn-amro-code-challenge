7c0ba10a6b52ef11ce6bbdd877810591286275e9
Tom Nchinda <tom@passionatepeople.io>
2025-04-07 07:23:15 +0200

    ✨feat(abnamro|pages): overview and product detail pages

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,14 +1,74 @@
+<script lang="ts" setup>
+import { onMounted, watch } from 'vue';
+import { useTvMaze } from '@/stores/shows';
+import Stars from '@/components/Stars.vue';
+
+const showsStore = useTvMaze();
+
+onMounted(() => {
+  showsStore.getShows();
+});
+</script>
+
 <template>
-  <div class="login">
-    This is my overview page
-  </div>
+  <main class="w-[100vw] overflow-hidden"
+    v-if="showsStore.randomShowCast && showsStore.randomShow && showsStore.randomShowBanner">
+    <div class="relative w-[100%] h-[400px] overflow-hidden">
+      <img :src="showsStore.randomShowBanner ?? ''" :alt="`Poster of ${showsStore.randomShow.name}`"
+        class="relative object-cover min-w-[100%] min-h-[100%] top-[50%] left-[50%] transform-[translate(-50%,-50%)]" />
+      <article
+        class="absolute left-0 top-0 bg-[rgba(0,0,0,0.5)] p-[10px] w-[250px] h-[100%] flex flex-col gap-[10px] overflow-hidden">
+        <h1 class="font-bold text-[24px]">{{ showsStore.randomShow?.name }}</h1>
+        <Stars :rating="showsStore.randomShow.rating.average" />
+        <div>
+          {{ showsStore.randomShow.runtime }} minutes - {{ showsStore.randomShow.genres.join(', ') }}
+        </div>
+        <div class="grow-[2] ellipsis-text">
+          <div class="font-bold">Starring</div>
+          {{ showsStore.randomShowCast.join(', ') }}
+        </div>
+        <RouterLink :to="`/shows/${showsStore.randomShow.id}`"
+          class="absolute bottom-0 left-0 w-[100%] h-[30px] bg-[rgba(255,0,0,0.5)] font-bold text-center content-center gradient">
+          More info
+        </RouterLink>
+      </article>
+    </div>
+    <div class="p-[40px]">
+      <section v-for="item in showsStore.showsByGenre" :key="item.genre" class="mb-[40px]">
+        <span class="flex flex-row items-center gap-[10px]">
+          <h2 class="font-bold text-[24px] genre__title">{{ item.genre }}</h2>
+          <span class="h-[2px] grow-[2] bg-[rgba(255,255,255,1)]"></span>
+        </span>
+        <span class="block w-[100%] overflow-x-auto">
+          <ul class="mb-[20px] gap-[10px] grid grid-flow-col overflow-x-auto w-[fit-content]">
+            <li v-for="show in item.shows" :key="show.id"
+              class="relative overflow-hidden h-[225px] w-[150px] object-cover cursor-pointer inline-block overflow-auto">
+              <RouterLink :to="`/shows/${show.id}`" class="absolute top-0 left-0 w-[100%] h-[100%]">
+                <h3 class="font-bold text-center content-center">{{ show.name }}</h3>
+                <img :src="show.image.original" :alt="`Poster of ${show.name}`"
+                  class="object-cover min-w-[100%] min-h-[100%]" />
+                <h3
+                  class="absolute top-0 left-0 w-[100%] h-[100%] bg-[rgba(0,0,0,0.5)] font-bold text-center content-center opacity-[0] hover:opacity-[1] transition duration-300 ease-out">
+                  {{ show.name }}
+                </h3>
+              </RouterLink>
+            </li>
+          </ul>
+        </span>
+      </section>
+    </div>
+  </main>
 </template>
 
-<style>
-.login {
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  align-items: center;
+<style scoped>
+.ellipsis-text {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 10;
+  overflow: hidden;
+}
+
+.gradient {
+  background: linear-gradient(to bottom, rgba(45, 193, 191, 0.5), rgba(45, 193, 191, 1));
 }
 </style>
---
+++ src/views/ShowDetails.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+import { onMounted, watch } from 'vue';
+import { useTvMaze } from '@/stores/shows';
+import Stars from '@/components/Stars.vue';
+import { useRoute } from 'vue-router';
+
+const showsStore = useTvMaze();
+const route = useRoute();
+
+onMounted(() => {
+  showsStore.getDetails(Number(route.path.split('/').pop()))
+    .then((results) => {
+      console.warn('/!\\\n showsStore2\n', { results });
+    });
+});
+</script>
+<template>
+  <main class="w-[100vw] overflow-hidden flex flex-col gap-[20px] min-h-[100vh]"
+    v-if="showsStore.showDetails && showsStore.detailsBanner">
+    <section class="relative w-[100%] h-[400px] overflow-hidden">
+      <img :src="showsStore.detailsBanner ?? ''" :alt="`Poster of ${showsStore.showDetails.show.name}`"
+        class="relative object-cover min-w-[100%] min-h-[100%] top-[50%] left-[50%] transform-[translate(-50%,-50%)]" />
+      <article class="absolute left-0 top-0 bg-[rgba(0,0,0,0.5)] p-[10px] w-[250px] h-[100%] flex flex-col gap-[10px]">
+        <RouterLink to="/browse">
+          <svg class="h-[15px] w-[15px] inline transform-[translateY(-1px)]" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32" fill="#ffffff">
+            <path
+              d="M 19.03125 4.28125 L 8.03125 15.28125 L 7.34375 16 L 8.03125 16.71875 L 19.03125 27.71875 L 20.46875 26.28125 L 10.1875 16 L 20.46875 5.71875 Z">
+            </path>
+          </svg>
+          BACK
+        </RouterLink>
+        <h1 class="font-bold text-[24px] whitespace-nowrap">{{ showsStore.showDetails.show.name }}</h1>
+        <img :src="showsStore.showDetails.show.image?.medium" :alt="`Poster of ${showsStore.showDetails.show.name}`"
+          class="h-[200px] w-[calc(200px*2/3)]" />
+        <Stars :rating="showsStore.showDetails.show.rating.average" />
+        <div>
+          {{ showsStore.showDetails.show.runtime }} minutes - {{ showsStore.showDetails.show.genres.join(', ') }}
+        </div>
+      </article>
+    </section>
+    <section class="grow-[2] p-[20px]">
+      <span class="block pb-[40px]">
+        <h2 class="font-bold text-[24px] mb-[20px]">Synopsis</h2>
+        <div v-html="showsStore.showDetails.show.summary"></div>
+      </span>
+
+      <span class="block pb-[40px]">
+        <h2 class="font-bold text-[24px] mb-[20px]">Cast</h2>
+        <ul class="flex flex-row flex-wrap gap-[50px]">
+          <li v-for="cast in showsStore.showDetails.cast" :key="cast.person.id"
+            class="flex flex-row gap-[10px] mb-[10px]">
+            <img :src="cast.person.image?.medium" :alt="`Poster of ${cast.person.name}`"
+              class="w-[75px] h-[75px] object-cover rounded-full" />
+            <div class="flex flex-col justify-center">
+              <h3 class="font-bold">{{ cast.person.name }}</h3>
+              <p>({{ cast.character.name }})</p>
+            </div>
+          </li>
+        </ul>
+      </span>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.ellipsis-text {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 10;
+  overflow: hidden;
+}
+</style>
