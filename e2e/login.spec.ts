c7dccc43ea2e238c8f5eea8d5a040495c5436b02
Tom Nchinda <tom@passionatepeople.io>
2025-04-07 12:45:09 +0200

    🚨test(abnamro|e2e): add Playwright tests for overview page

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('visits the app root url', async ({ page }) => {
+test('visits the login page', async ({ page }) => {
   await page.goto('/login');
   await expect(page.locator('h1')).toHaveText('Sign in with:');
   await expect(page.locator('button')).toHaveText('Google');
---
+++ e2e/overview.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('logs in with a Google account and goes to the overview page', async ({ page }) => {
+  await page.goto('/login');
+
+
+
+  const [popup] = await Promise.all([
+    page.context().waitForEvent('page'),
+    page.click('button:has-text("Google")'),
+  ]);
+
+  // Interact with the Google login popup
+  await popup.fill('input[type="email"]', 'magic.pptest@gmail.com');
+  await popup.click('button:has-text("Next")');
+  await popup.fill('input[type="password"]', 'Pa$5ionatepeople');
+  await popup.click('button:has-text("Next")');
+
+  await page.waitForURL('**/browse');
+
+  expect(page.url()).toContain('/browse');
+
+  await expect(page.locator('input')).toHaveAttribute('placeholder');
+
+  const placeholderText = await page.locator('input').getAttribute('placeholder');
+  await expect(placeholderText).toBe('Search for a show...');
+});
