096f234f9fd447b608f95dec7ce16168f9743556
Tom Nchinda <tom@passionatepeople.io>
2025-04-09 11:42:05 +0200

    ✨feat(abnamro|component): added new LazyImage component to make sure all images are managed and optimised in the same way

--- src/components/LazyImage.vue
+++ src/components/LazyImage.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
 const fallbackImage = '/images/abn-amro_logo.jpeg';
 const fallbackText = 'fallback image';
 
@@ -10,15 +12,22 @@ const props = defineProps<{
 
 const imageSrc = props.src || props.fallback || fallbackImage;
 const imageAlt = props.alt || fallbackText;
+const loaded = ref(false);
 
 const handleLoadingError = (event: Event) => {
   const element = event.currentTarget as HTMLImageElement;
   element.src = props.fallback || fallbackImage;
   element.setAttribute('alt', fallbackText);
 };
+
+const handleLoaded = (event: Event) => {
+  loaded.value = true;
+};
 </script>
 
 <template>
-  <img :src="imageSrc" :alt="imageAlt" loading="lazy" class="object-cover" @error="handleLoadingError" />
+  <img :src="imageSrc" :alt="imageAlt" loading="lazy"
+    :class="['object-cover', 'transition-all', 'duration-300', loaded ? 'opacity-[1]' : 'opacity-[0]']"
+    @error="handleLoadingError" @load="handleLoaded" />
 </template>
 <style scoped></style>
