0a745c3784b09a3bfa28737ae417dded552c1530
Tom Nchinda <tom@passionatepeople.io>
2025-04-10 12:07:48 +0200

    📦refactor(abnamro|auth): use fake user instead of real Google auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,6 @@
 import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router';
-import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
 import HomeView from '../views/HomeView.vue';
 import NotFound from '../views/NotFoundView.vue';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyApavUqR6DH4c96Es63CeOz4OizYDT8FhM",
-  authDomain: "abn-amro-code-challenge-51ac2.firebaseapp.com",
-  projectId: "abn-amro-code-challenge-51ac2",
-  storageBucket: "abn-amro-code-challenge-51ac2.firebasestorage.app",
-  messagingSenderId: "85973761352",
-  appId: "1:85973761352:web:dc2e3ea1e61317a5ff877d",
-  measurementId: "G-Q1PCF6TQX4"
-};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -68,26 +56,21 @@ const router = createRouter({
   ],
 });
 
-router.beforeEach(async (to: RouteLocationNormalized, from, next) => {
-  initializeApp(firebaseConfig);
-  const auth = getAuth();
+router.beforeEach(async (to: RouteLocationNormalized, _, next) => {
   const requiresAuth = to.matched.some((record) => {
     return record.meta.requiresAuth;
   });
 
-  const user: User | null = await new Promise((resolve) => {
-    onAuthStateChanged(auth, (user) => {
-      resolve(user);
-    });
-  });
+  const cookieList = document.cookie.split('; ');
+  const isLoggedIn = !!cookieList.find((cookie) => cookie === 'abnFlixUser=FakeUser');
 
   if (requiresAuth) {
-    if (!user) {
+    if (!isLoggedIn) {
       next('/login');
     } else {
       next();
     }
-  } else if (['/', '/login'].includes(to.path) && user) {
+  } else if (['/', '/login'].includes(to.path) && isLoggedIn) {
     next('/browse');
   } else {
     next();
